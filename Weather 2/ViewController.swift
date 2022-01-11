31c6f8fb678c052cdf30039b45ec30400d823eed
Vadim Yakimov <vadim_yakimov@me.com>
2022-01-11 21:06:04 +0300

    Initial Commit

---
+++ Weather 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weather 2
+//
+//  Created by Вадим on 11.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
