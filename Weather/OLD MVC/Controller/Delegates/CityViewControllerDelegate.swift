1cb0ef20f1ec82344cababe871103b7f1939339a
Vadim <vadim_yakimov@me.com>
2024-12-21 11:01:04 +0300

    Deleted old file

--- Weather/OLD MVC/Controller/Delegates/CityViewControllerDelegate.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-import UIKit
-
-protocol OLDCityViewControllerDelegate: AnyObject {
-    func cityViewController(didUpdateCurrentWeatherFor city: City)
-    func cityViewController(didUpdateHourlyForecastFor city: City)
-    func cityViewController(didUpdateDailyForecastFor city: City)
-    func cityViewController(scrollViewDidScroll scrollView: UIScrollView)    
-}
